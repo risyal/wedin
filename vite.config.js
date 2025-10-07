5ebda8d0014f9230e14c157e020e363d988f9ada
risal <risal.syah@gmail.com>
2025-10-07 16:21:17 +0700

    update vite.comfig

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,12 @@ import react from '@vitejs/plugin-react'
 import path from "path";
 import { fileURLToPath } from "url";
 
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/wedin/',
+  base: '/wedin/', 
   resolve: {
     alias: {
       "@src": path.resolve(__dirname, "src"),
