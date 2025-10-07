010e7a6aafe33b47a702f970b057940b5615ef91
risal <risal.syah@gmail.com>
2025-10-07 16:39:58 +0700

    update no jeckyl

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,34 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path";
 import { fileURLToPath } from "url";
+import { writeFileSync } from "fs";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/wedin/', 
+  plugins: [
+    react(),
+    {
+      name: "nojekyll-plugin",
+      closeBundle: () => {
+        writeFileSync("dist/.nojekyll", "");
+      },
+    },
+  ],
+  base: '/wedin/',
   resolve: {
     alias: {
       "@src": path.resolve(__dirname, "src"),
     },
   },
-})
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].[hash].js`,
+        chunkFileNames: `assets/[name].[hash].js`,
+        assetFileNames: `assets/[name].[hash].[ext]`
+      },
+    },
+  },
+});
